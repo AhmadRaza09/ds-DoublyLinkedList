c7f2b5913a1f3e0463a1e6d95a70e32d75d7906d
Ahmad Raza <ahmadrazamul09@gmail.com>
2022-03-22 11:25:44 +0500

    added DoublyLinkedList class. closes #2.

---
+++ DoublyLinkedList.java
@@ -0,0 +1,19 @@
+/**
+	Created By: Ahmad Raza
+	Date: 22 Mar 2022
+**/
+
+public class DoublyLinkedList
+{
+	//store the refrence of the first node
+	private Node head;
+	
+	//store the refrence of the last node
+	private Node tail;
+	
+	//store the refrence of the current node this node move anywhere in the chain
+	private Node current;
+	
+	//store the size of the node in the chain
+	private int size;
+}
